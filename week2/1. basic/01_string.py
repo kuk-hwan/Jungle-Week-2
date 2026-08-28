1d8541f2e19057c9d6296f831b14484bf28ff24d
kuk-hwan <xi.crebig@gmail.com>
2026-08-28 16:56:03 +0900

    TODO

--- week2/1. basic/01_string.py
+++ week2/1. basic/01_string.py
@@ -25,8 +25,6 @@
 """
 
 def is_palindrome(s: str) -> bool:
-    filtered = "".join(c.lower() for c in s if c.isalnum())
-    return filtered == filtered[::-1]
 
     """
     문자열이 회문인지 판별하는 함수
@@ -39,12 +37,12 @@ def is_palindrome(s: str) -> bool:
     """
     # TODO: 알파벳과 숫자만 남기고 소문자로 변환하세요
     # 힌트: isalnum() 메서드와 lower() 메서드 사용
-    pass
+    filtered = "".join(c.lower() for c in s if c.isalnum())
     
     # TODO: 정제된 문자열이 회문인지 확인하세요
     # 방법1: 문자열을 뒤집어서 비교 ([::-1] 사용)
     # 방법2: 양 끝 인덱스를 이용한 투 포인터 방식
-    pass
+    return filtered == filtered[::-1]
     
     #return False
 
