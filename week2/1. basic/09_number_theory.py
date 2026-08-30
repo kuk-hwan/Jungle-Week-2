cc6a9d4d5ddb3da9a99760c6c645ac94a90a6c2f
kuk-hwan <xi.crebig@gmail.com>
2026-08-30 11:31:30 +0900

    1. basic

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -114,9 +114,17 @@ def is_prime(n):
 
     if n < 2:
         return False
-    for i in range(2, int(math.sqrt(n)) + 1):
+    
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         if n % i == 0:
             return False
+
     return True
 
 # 테스트 케이스
