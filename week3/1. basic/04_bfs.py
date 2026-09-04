02c7662945d37f227fc19156d717f5b1df7ef693
kuk-hwan <xi.crebig@gmail.com>
2026-09-04 19:25:33 +0900

    graph

--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -47,13 +47,21 @@ def bfs(graph, start):
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    queue = deque([start])
+    visited.append(start)
+
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
+    while queue:
+        v = queue.popleft()
+        
+        for i in graph[v]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
     
     return visited
 
